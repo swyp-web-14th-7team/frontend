9e614e5f14c965250c4f37f911bdabd142e35873
Dannii0421 <danhui4090@daum.net>
2026-07-05 20:45:27 +0900

    feat: create login page UI

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,13 @@
 const Login = () => {
     return (
-    <div>
-        Login Page
-    </div>
+    <main>
+        <h1>Nodi</h1>
+        <p>나를 한 장의 카드로, 더 쉽고 인상 깊은 첫 만남</p>
+
+        <button type="button">카카오톡으로 계속하기</button>
+        <button type="button">구글로 계속하기</button>
+        <button type="button">네이버로 계속하기</button>
+    </main>
     );
 };
 
